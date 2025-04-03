fe03b9ed5c3280090d1389aff6e3800728f1323e
Kastire <104029768+Kastire96@users.noreply.github.com>
2025-04-03 11:11:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
 
+# Funkcja wyświetlająca planszę gry
 def print_board(board):
     for row in board:
         print(" | ".join(row))
     print()
 
 def check_winner(board, player):
+    # Sprawdzenie wierszy
     for row in board:
         if all(cell == player for cell in row):
             return True
